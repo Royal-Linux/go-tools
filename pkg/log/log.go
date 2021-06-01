8775d8066aadb8bd94935274428bc8b480024bc9
ulises-jeremias <ulisescf.24@gmail.com>
2021-06-01 00:52:14 -0300

    Added logs pkg

---
+++ pkg/log/log.go
@@ -0,0 +1,60 @@
+package log
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/Royal-Linux/hornero/pkg/config"
+	"github.com/Royal-Linux/logrus"
+)
+
+// NewLogger returns a new logger
+func NewLogger(config *config.AppConfig, rollrusHook string) *logrus.Entry {
+	var log *logrus.Logger
+	if config.Debug || os.Getenv("DEBUG") == "TRUE" {
+		log = newDevelopmentLogger(config)
+	} else {
+		log = newProductionLogger()
+	}
+
+	// highly recommended: tail -f development.log | humanlog
+	// https://github.com/aybabtme/humanlog
+	log.Formatter = &logrus.JSONFormatter{}
+
+	return log.WithFields(logrus.Fields{
+		"debug":     config.Debug,
+		"version":   config.Version,
+		"commit":    config.Commit,
+		"buildDate": config.BuildDate,
+	})
+}
+
+func getLogLevel() logrus.Level {
+	strLevel := os.Getenv("LOG_LEVEL")
+	level, err := logrus.ParseLevel(strLevel)
+	if err != nil {
+		return logrus.DebugLevel
+	}
+	return level
+}
+
+func newDevelopmentLogger(config *config.AppConfig) *logrus.Logger {
+	log := logrus.New()
+	log.SetLevel(getLogLevel())
+	file, err := os.OpenFile(filepath.Join(config.ConfigDir, "development.log"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("unable to log to file")
+		os.Exit(1)
+	}
+	log.SetOutput(file)
+	return log
+}
+
+func newProductionLogger() *logrus.Logger {
+	log := logrus.New()
+	log.Out = ioutil.Discard
+	log.SetLevel(logrus.ErrorLevel)
+	return log
+}
